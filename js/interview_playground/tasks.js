a03a8c4cc16f4a19a3053b8f264b9af48e40a8a5
oleg shchepotkin <music.wave.form7@gmail.com>
2024-07-17 16:09:55 +0300

    (playground) delay

--- js/interview_playground/tasks.js
+++ js/interview_playground/tasks.js
@@ -13,12 +13,17 @@ console.log(name.givePizzaToHim())
 
 
 // 2 what diameter and perimeter will print
+
+function diameter() {
+    return this.radius * 2;
+};
+
 const shape = {
     radius: 10,
     diameter() {
         return this.radius * 2;
     },
-    perimeter: () => 2 * Math.PI * this.radius,
+    perimeter: (() => 2 * Math.PI * this.radius).bind({radius: 10}),
 }
 
 console.log(shape.diameter());
@@ -29,3 +34,31 @@ console.log(shape.perimeter());
 shape.perimeter = function() { return 2 * Math.PI * this.radius };
 console.log(shape.diameter());
 console.log(shape.perimeter());
+
+
+// write delay function implementation
+
+delay(5000).then(() => console.log('Hello !'));
+
+
+function delay(interval){
+    return new Promise((resolve, reject) => {
+        let progress = true;
+        let progressCount = 1;
+        setTimeout(() => {progress = false; resolve();} , interval);
+        let timeout = setInterval(() => {
+            if(!progress){
+                clearTimeout(timeout);
+                return;
+            }
+            let leftOver = progressCount % 4;
+            process.stdout.clearLine();  // clear current text
+            process.stdout.cursorTo(0);  // move cursor to beginning of line
+            process.stdout.write(Array(leftOver).fill(6).reduce((acc) => {
+                return acc+'.';
+            },'') + '\r');
+            progressCount++;
+            
+        }, 300)
+    }) 
+}
