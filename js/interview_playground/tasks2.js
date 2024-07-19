3315b31ab8159236e824f88d6dc82389924589e0
oleg shchepotkin <music.wave.form7@gmail.com>
2024-07-19 12:06:13 +0300

    (playground):2nd tasks file

---
+++ js/interview_playground/tasks2.js
@@ -0,0 +1,25 @@
+async function f() {
+    console.log(1);
+    const promise = new Promise((resolve) => {
+        console.log(2);
+        setTimeout(() => {
+            console.log(3);
+            resolve('готово!')
+            console.log(4);
+        })
+    });
+
+    console.log(5);
+
+    const result = await promise;
+    console.log(6);
+    console.log(result);
+    return 'Result';
+}
+
+f();
+console.log(7);
+
+// what does it print?
+
+// 1,2,5,7,3,4,6,готово!
