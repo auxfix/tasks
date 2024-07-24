bcf596dae6f44286827f03d24ce75d7a3bf74782
oleg shchepotkin <music.wave.form7@gmail.com>
2024-07-24 10:39:36 +0300

    (curring): task 3

--- js/interview_playground/tasks3.js
+++ js/interview_playground/tasks3.js
@@ -3,10 +3,13 @@ function test(a, b, c, d, e){
 }
 
 const testC  = curring(test);
+const testC2  = curring2(test);
+const testC3  = curring2(test);
 
 
 console.log(testC(1)(2)(3,4)(1)(3)(4)) // 11 - Too many arguments
-
+console.log(testC2(1, 2, 3, 4, 1)); // 11
+console.log(testC3(1)(2)(3,4)(1)) // 11
 
 function curring(func) {
     let flength = func.length;
@@ -17,6 +20,7 @@ function curring(func) {
             console.log('Too many arguments')
             return cur;
         };
+
         let argsToCall = [
             ...argsInternal, 
             ...(new Array(flength - argsInternal.length)).fill(0)
@@ -25,5 +29,17 @@ function curring(func) {
         console.log(func(...argsToCall));
 
         return cur;
+    }
+}
+
+function curring2(func){
+    let allArgs = [];
+    return function cur(...args) {
+        allArgs = [...allArgs, ...args]
+        if(allArgs.length === func.length) {
+            return func(...allArgs);
+        } else {
+            return cur;
+        }
     }
 }
