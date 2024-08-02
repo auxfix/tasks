390bd61780efc0038206ef67e02a406204ea8863
oleg shchepotkin <music.wave.form7@gmail.com>
2024-08-02 14:20:17 +0300

    (tasks): type of tasks

---
+++ js/int_play_2/tasks.js
@@ -0,0 +1,8 @@
+// what each of these typeofs prints
+
+console.log(typeof NaN);        // number
+console.log(typeof null);       // object
+console.log(typeof []);         // object
+console.log(typeof Array);      // function
+console.log(typeof (() => {})); // function 
+console.log(typeof Function);   // function
