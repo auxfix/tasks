1273279688005dcc7d1a446503c6513f107835e3
oleg shchepotkin <music.wave.form7@gmail.com>
2024-08-03 10:17:33 +0300

    (tasks): comapre types

--- js/int_play_2/tasks.js
+++ js/int_play_2/tasks.js
@@ -6,3 +6,18 @@ console.log(typeof []);         // object
 console.log(typeof Array);      // function
 console.log(typeof (() => {})); // function 
 console.log(typeof Function);   // function
+
+
+// what these evaluation will print
+
+console.log([] == [])  // false
+console.log([] === []) // false
+
+console.log({} == {})  // false
+console.log({} === {}) // false
+
+console.log(NaN == NaN)  // false
+console.log(NaN === NaN) // false
+console.log(null == undefined)  // true
+console.log(undefined == null) // true
+console.log(undefined === null) // false
