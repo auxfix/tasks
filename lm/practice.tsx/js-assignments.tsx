c6871099cfe12fdb2aeb4e0e8dd2a6637edfb5d6
Oleg Shchepotkin <music.wave.form7@gmail.com>
2024-10-28 10:16:08 +0300

    (int): new practice

--- lm/practice.tsx/js-assignments.tsx
+++ lm/practice.tsx/js-assignments.tsx
@@ -51,3 +51,15 @@ const fetchUrl = (url) => {
   console.log(replace("I love sunday. Because  every sunday i meet my parents.", 'sunday', "saturday"));
   // Result: I love saturday. Because every Saturday i meet my parents.
   // -----------------------------------------
+
+  // Find missing number in array
+
+  function missed(array) {
+    // может быть не сортирован
+    // всегда валиден и содержит 1 пропущенное число
+    // всегда массив чисел
+    // вернуть пропущенное число в ряду чисел
+  }
+  console.log(missed([0, 1, 3, 4])); // 2
+  console.log(missed([-2, 0, 1, 2])); // -1
+  console.log(missed([1, 3, 2, -1])); // 0
